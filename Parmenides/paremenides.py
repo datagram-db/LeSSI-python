baa88d4c2996f68e25224a1b85298553d15f4699
Giacomo Bergami <bergamigiacomo@gmail.com>
2024-05-16 12:07:43 +0100

    «Doc-Doc» «Chi è?»

--- Parmenides/paremenides.py
+++ Parmenides/paremenides.py
@@ -6,11 +6,17 @@ __version__ = "2.0"
 __maintainer__ = "Giacomo Bergami"
 __email__ = "bergamigiacomo@gmail.com"
 __status__ = "Production"
+
+import re
+from string import Template
 import rdflib
 from rdflib.graph import Graph, ConjunctiveGraph
 from rdflib import Graph, URIRef, BNode, Literal, XSD
 from rdflib import Namespace
 from rdflib.namespace import OWL, RDF, RDFS, FOAF
+
+def instantiateWithMap(s:str, m:dict):
+    return Template(s.replace("@","$")).safe_substitute(m)
 
 class Parmenides():
      parmenides_ns = Namespace("https://lo-ds.github.io/parmenides#")
@@ -82,7 +88,165 @@ class Parmenides():
                  k[type[1:]] = str(d.get("dst"))[len(Parmenides.parmenides_ns):]
              yield k
 
+     def single_edge_dst_unary_capability(self, src, edge_type, verb, subj):
+         knows_query = """
+         SELECT DISTINCT ?src ?edge_type ?dst ?src_label ?verb ?subj
+         WHERE {
+             ?src ?edge_type ?dst.
+             ?dst parmenides:entryPoint ?verb_e.
+             ?verb_e rdfs:label ?verb. 
+             ?dst parmenides:subject ?subj_e.
+             ?subj_e rdfs:label ?subj. 
+             ?src rdfs:label ?src_label.
+         }"""
+         bindings = {}
+         srcBool = False
+         edgeBool = False
+         verbBool = False
+         subjBool = False
+         objBool = False
+         if not src.startswith("^"):
+             bindings["src_label"] = Literal(src, datatype=XSD.string)
+         else:
+             srcBool = True
+         if not edge_type.startswith("^"):
+             bindings["edge_type"] = URIRef(Parmenides.parmenides_ns[edge_type])
+         else:
+             edgeBool = True
+         if not verb.startswith("^"):
+             bindings["verb"] = Literal(verb, datatype=XSD.string)
+         else:
+             verbBool = True
+         if not subj.startswith("^"):
+             bindings["subj"] = Literal(subj, datatype=XSD.string)
+         else:
+             subjBool = True
+         qres = self.g.query(knows_query, initBindings=bindings)
+         for x in qres:
+            d = x.asdict()
+            k = dict()
+            if srcBool:
+                k[src[1:]] = str(d.get("src_label"))
+            if subjBool:
+                k[subj[1:]] = str(d.get("subj"))
+            if verbBool:
+                k[verb[1:]] = str(d.get("verb"))
+            if edgeBool:
+                k[edge_type[1:]] = str(d.get("edge_type"))[len(Parmenides.parmenides_ns):]
+            yield k
+
+     def single_edge_dst_binary_capability(self, src, edge_type, verb, subj, obj):
+         knows_query = """
+         SELECT DISTINCT ?src ?edge_type ?dst ?src_label ?verb ?subj ?obj
+         WHERE {
+             ?src ?edge_type ?dst.
+             ?dst parmenides:entryPoint ?verb_e.
+             ?verb_e rdfs:label ?verb. 
+             ?dst parmenides:subject ?subj_e.
+             ?subj_e rdfs:label ?subj. 
+             ?dst parmenides:d_object ?obj_e.
+             ?obj_e rdfs:label ?obj. 
+             ?src rdfs:label ?src_label.
+         }"""
+         bindings = {}
+         srcBool = False
+         edgeBool = False
+         verbBool = False
+         subjBool = False
+         objBool = False
+         if not src.startswith("^"):
+             bindings["src_label"] = Literal(src, datatype=XSD.string)
+         else:
+             srcBool = True
+         if not edge_type.startswith("^"):
+             bindings["edge_type"] = URIRef(Parmenides.parmenides_ns[edge_type])
+         else:
+             edgeBool = True
+         if not verb.startswith("^"):
+             bindings["verb"] = Literal(verb, datatype=XSD.string)
+         else:
+             verbBool = True
+         if not subj.startswith("^"):
+             bindings["subj"] = Literal(subj, datatype=XSD.string)
+         else:
+             subjBool = True
+         if not obj.startswith("^"):
+             bindings["obj"] = Literal(obj, datatype=XSD.string)
+         else:
+             objBool = True
+         qres = self.g.query(knows_query, initBindings=bindings)
+         for x in qres:
+            d = x.asdict()
+            k = dict()
+            if srcBool:
+                k[src[1:]] = str(d.get("src_label"))
+            if subjBool:
+                k[subj[1:]] = str(d.get("subj"))
+            if objBool:
+                k[obj[1:]] = str(d.get("obj"))
+            if verbBool:
+                k[verb[1:]] = str(d.get("verb"))
+            if edgeBool:
+                k[edge_type[1:]] = str(d.get("edge_type"))[len(Parmenides.parmenides_ns):]
+            yield k
+
+     def single_edge_src_multipoint(self, src, src_spec, edge_type, dst):
+         knows_query = """
+         SELECT DISTINCT ?src ?edge_type ?dst ?src_label ?src_spec ?dst_label
+         WHERE {
+             ?src ?edge_type ?dst.
+             ?src parmenides:entryPoint ?src_entry.
+             ?src_entry rdfs:label ?src_label.
+             ?src_multi parmenides:hasAdjective ?src_spec_node.
+             ?src_spec_node rdfs:label ?src_spec.
+             ?dst rdfs:label ?dst_label .
+         }"""
+         bindings = {}
+         srcBool = False
+         srcSpecBool = False
+         edgeBool = False
+         dstBool = False
+         if not src.startswith("^"):
+             bindings["src_label"] = Literal(src, datatype=XSD.string)
+         else:
+             srcBool = True
+         if not src_spec.startswith("^"):
+             bindings["src_spec"] = Literal(src_spec, datatype=XSD.string)
+         else:
+             srcSpecBool = True
+         if not edge_type.startswith("^"):
+             bindings["edge_type"] = URIRef(Parmenides.parmenides_ns[edge_type])
+         else:
+             edgeBool = True
+         if not dst.startswith("^"):
+             bindings["dst_label"] = Literal(dst, datatype=XSD.string)
+         else:
+             dstBool = True
+         qres = self.g.query(knows_query, initBindings=bindings)
+         for x in qres:
+            d = x.asdict()
+            k = dict()
+            if srcBool:
+                k[src[1:]] = str(d.get("src_label"))
+            if srcSpecBool:
+                k[src_spec[1:]] = str(d.get("src_spec"))
+            if dstBool:
+                k[dst[1:]] = str(d.get("dst_label"))
+            if edgeBool:
+                k[edge_type[1:]] = str(d.get("edge_type"))[len(Parmenides.parmenides_ns):]
+            yield k
+
      def single_edge(self, src, edge_type, dst):
+         m = re.match(r"(?P<main>[^\[]+)\[(?P<spec>[^\]]+)\]", src)
+         if m:
+             yield from self.single_edge_src_multipoint(m.group('main'), m.group('spec'), edge_type, dst)
+             return
+         m = re.match(r"(?P<main>[^\(]+)\((?P<subj>[^\,)]+),(?P<obj>[^\)]+)\)", dst)
+         if m:
+             yield from self.single_edge_dst_binary_capability(src, edge_type, m.group('main'), m.group('subj'), m.group('obj'))
+         m = re.match(r"(?P<main>[^\(]+)\((?P<subj>[^\)]+)\)", dst)
+         if m:
+             yield from self.single_edge_dst_unary_capability(src, edge_type, m.group('main'), m.group('subj'))
          knows_query = """
          SELECT DISTINCT ?src ?edge_type ?dst ?src_label ?dst_label
          WHERE {
@@ -121,11 +285,12 @@ class Parmenides():
 
      @staticmethod
      def instantiate_query_with_map(Q, m):
+
          if Q is None:
              return None
          elif isinstance(Q, list):
              if len(Q)==2 or len(Q)==3:
-                 return list(map(lambda x: m.get(x[1:], "") if x.startswith("@") else x, Q))
+                 return list(map(lambda x: instantiateWithMap(x, m), Q))
              else:
                  raise ValueError("Len 2 are IsA queries, while the rest are Len 3")
          elif isinstance(Q, tuple) and len(Q)==2:
@@ -181,11 +346,16 @@ if __name__ == "__main__":
     #     ?a rdfs:label ?c .
     # }"""
     # qres = g.query(knows_query)
+    g.multiple_queries(tuple(["and", [["city", "hasProperty", "busy"], ["city"]]]))
     for hasEdge in g.single_edge("city center", "partOf", "^var"):
         print(hasEdge)
     for outcome in g.isA("flow", "^t"):
         print(outcome)
     for outcome in g.isA("busy", "Adjective"):
+        print(outcome)
+    for outcome in g.single_edge("city[busy]", "relatedTo", "^d"):
+        print(outcome)
+    for outcome in g.single_edge("traffic jam", "capableOf", "^v(^s,^o)"):
         print(outcome)
     # print(g.typeOf("Newcastle"))
     # print(g.typeOf("city"))
